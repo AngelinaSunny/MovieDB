f7f8abe0720743bfe5b54b8921ebed38fd27d60b
AngelinaSun <87764242+AngelinaSunny@users.noreply.github.com>
2022-10-06 15:19:34 +0300

    Add files via upload

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Input } from 'antd';
 
-const SearchInput = ({ onChangeSearch }) => (
-  
-  return <Input placeholder="Type to search..." onChange={onChangeSearch} />
-  
-);
+const SearchInput = ({ onChangeSearch }) => <Input placeholder="Type to search..." onChange={onChangeSearch} />;
 
 export default SearchInput;
