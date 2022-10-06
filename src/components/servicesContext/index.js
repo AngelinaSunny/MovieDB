9359afd3afbaed1ed6fb17db4778e381434bc9aa
AngelinaSun <87764242+AngelinaSunny@users.noreply.github.com>
2022-10-06 16:57:53 +0300

    Delete src/components/servicesContext directory

--- src/components/servicesContext/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const { Provider: MovieServiceProvider, Concumer: MovieServiceConsumer } = React.createContext();
-
-export default { MovieServiceProvider, MovieServiceConsumer };
