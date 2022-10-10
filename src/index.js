213c7832403bdf5a3acb89552f41226bcf6e0f81
AngelinaSun <87764242+AngelinaSunny@users.noreply.github.com>
2022-10-10 15:41:37 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 
 import App from './components/App/App';
 
